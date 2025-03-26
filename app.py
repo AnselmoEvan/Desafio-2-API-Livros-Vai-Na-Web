a801ba318595a200c6a0dcf5d656bc3edc1ad1ba
AnselmoEvan <anselmo.evan@gmail.com>
2025-03-26 18:43:23 -0300

    prontyo para deploy

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import sqlite3
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 def init_db():
@@ -23,7 +25,7 @@ init_db()
 
 @app.route('/')
 def home_page():
-    return "<h2>Minha primeira pagina utilizando o Flask!</h2>"
+    return render_template('index.html')
 
 
 @app.route("/doar", methods=["POST"])
@@ -63,6 +65,21 @@ def listar_livros():
         } 
         livros_formatados.append(dicionario_livros)
     return jsonify(livros_formatados)     
+
+
+@app.route('/livros/<int:livros_id>', methods=['DELETE'])
+def deletar_livro(Livro_id):
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM livros WHERE id = ?", (Livro_id,))
+        conn.commit()
+
+    if cursor.rowcount == 0:
+        return jsonify({'erro': 'Livros não encontrado'}), 404
+
+    return jsonify({'menssagem': 'Livro deletado!'}), 200    
+
+
 
 
 
